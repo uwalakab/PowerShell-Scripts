aeddc77b89ef00755a514fbc261f2bc584ee37d0
uwalakab <62111763+uwalakab@users.noreply.github.com>
2025-08-11 12:48:25 +0100

    Update Set-PDF-ICON.ps1

--- Set-PDF-Icon-for-Chrome/Set-PDF-ICON.ps1
+++ Set-PDF-Icon-for-Chrome/Set-PDF-ICON.ps1
@@ -1,4 +1,5 @@
-## The following Reg key has the following advanced permissions set for the current user [Set Value, Create Subkey]
+## Run the script as Administrator, or (less secure) set the following Reg Key with  the following Windows advanced permissions for the current user [Set Value, Create Subkey]
+
 New-ItemProperty -Path HKLM:\SOFTWARE\Classes\ChromePDF\DefaultIcon -Name "(default)" -PropertyType "String" -Value '"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe",11' -Force
 
 ## [HKEY_CLASSES_ROOT\ChromePDF\DefaultIcon]
